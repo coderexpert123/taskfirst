dce9072f60289536e29155a1a9ff73deb23e3c36
Nishant <nishantsingh2jan1998@gmail.com>
2020-10-07 20:21:16 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Credit Management System</title>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Credit Management System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+ 
+  </button>
+ 
+</nav>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://source.unsplash.com/500x100/?bank ,money
+" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://source.unsplash.com/500x100/?atm,bank
+." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://source.unsplash.com/500x100/?credit card,visa card
+" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<div class="container mt-10">
+  <div class="alert alert-primary text-center" role="alert">
+ Credit Manaement System 
+</div>
+</div>
+
+
+<div class="container">
+  <div class="row col d-flex justify-content-center mr-5" >
+  <div class="card" style="width: 18rem;" class="col-sm-1">
+  <img class="card-img-top" src="https://source.unsplash.com/500x900/?atm,banking" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title text-center">View User</h5>
+    <p class="card-text text-center">From here you see all te user that are Register to te bank.</p>
+    <a href="getdetail.php" class="btn btn-primary col d-flex justify-content-center" > StartNow </a>
+  </div>
+</div>
+
+ 
+  <div class="card" style="width: 18rem;" class="col-sm-1 mr-5 ml-5">
+  <img class="card-img-top" src="https://source.unsplash.com/500x900/?credit card,money " alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title text-center">Credit Transfer</h5>
+    <p class="card-text text-center">From here you Start Transfer Money Now..</p>
+  
+    <a href="selectuser.php" class="btn btn-primary col d-flex justify-content-center"  >Start Now</a>
+  </div>
+</div>
+
+</div>
+<!-- Footer -->
+<div class="container">
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center color-pink py-3 bg-primary mt-10">© 2020 Copyright: Credit management system
+  </div>
+  <!-- Copyright -->
+
+</footer>
+</div>
+<!-- Footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
